f4d1d5d29d02a8ef83ea9c939d9003e028cb0b4d
borosabel <boros.abel@geogebra.at>
2024-11-20 16:10:28 +0100

    go to stock details from search page on stock item click

--- peaktt/src/components/StockSuggestionItem.tsx
+++ peaktt/src/components/StockSuggestionItem.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
 import { StockSuggestion } from "@/types/alphaVantageTypes";
 
 interface StockSuggestionItemProps {
@@ -12,15 +15,29 @@ const StockSuggestionItem: React.FC<StockSuggestionItemProps> = ({
   onToggleFavorite,
   isFavorite,
 }) => {
+  const router = useRouter();
+
+  const handleStockClick = () => {
+    router.push(`/stock/${suggestion.symbol}`);
+  };
+
   return (
     <li className="py-2 border-b border-gray-300 flex justify-between items-center">
-      <div>
+      <div
+        onClick={handleStockClick}
+        className="cursor-pointer hover:underline"
+      >
         <span className="font-semibold">{suggestion.symbol}</span> -{" "}
         {suggestion.name}
       </div>
       <button
-        onClick={() => onToggleFavorite(suggestion.symbol)}
-        className={`px-3 py-1 ml-4 rounded-md ${isFavorite ? "bg-red-500 text-white" : "bg-gray-300"}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          onToggleFavorite(suggestion.symbol);
+        }}
+        className={`px-3 py-1 ml-4 rounded-md ${
+          isFavorite ? "bg-red-500 text-white" : "bg-gray-300"
+        }`}
       >
         {isFavorite ? "Unfavorite" : "Favorite"}
       </button>
